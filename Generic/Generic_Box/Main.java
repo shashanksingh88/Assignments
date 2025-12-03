84a2a93a35e6b189a85ac32ea0616ce597ba0d55
g5n9u1-coder <shashank.singh2_cs.aiml@gla.ac.in>
2025-12-03 12:15:34 +0530

    /*

--- Generic/Generic_Box/Main.java
+++
@@ -1,9 +0,0 @@
-package Generic.Generic_Box;
-
-public class Main {
-	public static void main(String[] args) {
-		Box<String> name = new Box<>();
-		name.set("Tanmay");
-		System.out.println(name.get());
-	}
-}
